0e479bd1334b62b0f2cf0f575e966590725571ca
tangyichao <874500641@qq.com>
2015-04-21 19:42:37 +0800

    添加关于功能

---
+++ src/com/innobuddy/SmartStudy/activity/FeedbackActivity.java
@@ -0,0 +1,17 @@
+package com.innobuddy.SmartStudy.activity;
+
+import com.innobuddy.SmartStudy.R;
+
+import android.os.Bundle;
+
+public class FeedbackActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_feedback);
+
+	}
+
+	
+}
